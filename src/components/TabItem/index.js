061741c0a976f161f4bcfb55c705d757d7f1a6e5
root <saikumarkotrangi>
2023-03-28 09:04:31 +0000

    appStore

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+export default class TabItem extends Component {
+  render() {
+    const {eachObject, key, updateState, isClicked} = this.props
+    const {displayText, tabId} = eachObject
+
+    const classname = isClicked ? 'btn-styles' : ''
+    console.log(isClicked)
+
+    this.onUpdate = () => {
+      updateState(tabId)
+    }
+
+    return (
+      <li key={key}>
+        <button
+          onClick={this.onUpdate}
+          className={`btn-style ${classname}`}
+          type="button"
+        >
+          {displayText}
+        </button>
+      </li>
+    )
+  }
+}
